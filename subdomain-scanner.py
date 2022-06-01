c632fe2a6dfbde2b87f17ac4a3fda912fb6e39b0
Nasrullah Pandu <103873184+pandudw@users.noreply.github.com>
2022-06-01 23:12:42 +0700

    added a try/except block
    added a try/except block to the code to check the availability of subdomains

--- subdomain-scanner.py
+++ subdomain-scanner.py
@@ -3,7 +3,7 @@ import requests
 # input the domain to scan subdomains
 domain= "tokopedia.com"
 
-# read all subdomains from file (subdomains.txt)
+# read all subdomains from wordlists
 file= open("subdomains.txt")
 
 # read all content 
@@ -11,3 +11,22 @@ content= file.read()
 
 # split to create a new lines
 subdomains= content.splitlines()
+
+# a list of discovered subdomains
+discovered_subdomains = []
+for subdomain in subdomains:
+
+# construct the url for subdomain
+    url= f"http://{subdomain}.{domain}"
+    
+# test to get the HTTP response from server
+    try: 
+        request.get(url)
+      
+# if this raises an error, that means the subdomain does not exist and just pass the process
+    except Exception:
+        pass
+    else:
+        print("[+] Discovered subdomain", url)
+# append the discovered subdomain
+        discovered_subdomains.append()
