b71380537b45aff5cc63e800ab97124c0a697901
Nasrullah Pandu <103873184+pandudw@users.noreply.github.com>
2024-02-02 16:30:03 +0000

    update subdomain enum to get data subdomains from domain registrar

--- subdomain-scanner.py
+++ subdomain-scanner.py
@@ -1,37 +1,7 @@
 import requests
 
-# input the domain to scan subdomains
-domain= "tokopedia.com"
+def get_subdomains(api_key, api_secret, domain):
+    url = 
+    headers = {'Authorization': f'sso-key {api_key}:{api_secret}'}
 
-# read all subdomains from wordlists
-file= open("subdomains.txt")
-
-# read all content 
-content= file.read()
-
-# split to create a new lines
-subdomains= content.splitlines()
-
-# a list of discovered subdomains
-discovered_subdomains = []
-for subdomain in subdomains:
-
-# construct the url for subdomain
-    url= f"http://{subdomain}.{domain}"
-    
-# test to get the HTTP response from server
-    try: 
-        request.get(url)
-      
-# if this raises an error, that means the subdomain does not exist and just pass the process
-    except Exception:
-        pass
-    else:
-        print("[+] Discovered subdomain", url)
-# append the discovered subdomain
-        discovered_subdomains.append()
-
-# save the discovered subdomains into a file
-with open("result", "w") as f:
-    for subdomain in discovered_subdomains:
-        print(subdomain, file=f)
+    response = requests.get(url, headers=headers)
